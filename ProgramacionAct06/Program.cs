ea431b60836967f1f55debf698841c76ca3fdf39
Raimon-itb <raimon.pera.7e9@itb.cat>
2025-10-16 12:36:41 +0200

    Entrega ejercicio 1.

--- ProgramacionAct06/Program.cs
+++ ProgramacionAct06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    namespace sumaEnters
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            const string Msg_input1 = "Enter the first integer: ";
+            const string Msg_input2 = "Enter the second integer: ";
+            const string Msg_result = "The sum is: ";
+
+            int num1, num2, sum;
+            Console.WriteLine(Msg_input1);
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Msg_input2);
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+
+            Console.WriteLine(Msg_result + sum);
+        }
+    }
+}
